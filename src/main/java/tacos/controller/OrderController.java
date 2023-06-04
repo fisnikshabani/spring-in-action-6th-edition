cda6f579b30cb44f3e96a35870754a2dd9ad9485
Fisnik Shabani <fisnik.shabani@gmail.com>
2023-06-04 14:54:52 +0200

    Inserting data.

--- src/main/java/tacos/controller/OrderController.java
+++ src/main/java/tacos/controller/OrderController.java
@@ -4,10 +4,14 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.bind.support.SessionStatus;
 import tacos.model.TacoOrder;
+import tacos.repository.OrderRepository;
+
+import javax.validation.Valid;
 
 @Slf4j
 @Controller
@@ -15,18 +19,25 @@ import tacos.model.TacoOrder;
 @SessionAttributes("tacoOrder")
 public class OrderController {
 
+    private OrderRepository orderRepository;
+
+    public OrderController(OrderRepository orderRepository) {
+        this.orderRepository = orderRepository;
+    }
+
     @GetMapping("/current")
     public String orderForm() {
         return "orderForm";
     }
 
-    public String processOrder(TacoOrder order, Errors errors, SessionStatus sessionStatus) {
+    @PostMapping
+    public String processOrder(@Valid TacoOrder order, Errors errors, SessionStatus sessionStatus) {
 
         if (errors.hasErrors()) {
             return "orderForm";
         }
 
-        log.info("Order submitted: {}", order);
+        orderRepository.save(order);
         sessionStatus.setComplete();
 
         return "redirect:/";
---
+++ src/main/java/tacos/model/IngredientRef.java
@@ -0,0 +1,9 @@
+package tacos.model;
+
+import lombok.Data;
+
+@Data
+public class IngredientRef {
+
+    private final String ingredient;
+}
