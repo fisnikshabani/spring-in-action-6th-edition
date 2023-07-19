51cadf6bffd606556c06e325deb5f10c6924eddc
Fisnik Shabani <fisnik.shabani@gmail.com>
2023-07-20 00:41:11 +0200

    Implementing POST, PATCH and DELETE endpoints

--- src/main/java/tacos/controller/TacoController.java
+++ src/main/java/tacos/controller/TacoController.java
@@ -1,11 +1,14 @@
 package tacos.controller;
 
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import tacos.model.Taco;
+import tacos.model.TacoOrder;
+import tacos.repository.OrderRepository;
 import tacos.repository.TacoRepository;
 
 import java.util.Optional;
@@ -16,6 +19,8 @@ import java.util.Optional;
 public class TacoController {
 
     private TacoRepository tacoRepository;
+
+    private OrderRepository orderRepository;
 
     public TacoController(TacoRepository tacoRepository) {
         this.tacoRepository = tacoRepository;
@@ -33,5 +38,50 @@ public class TacoController {
     public ResponseEntity<Taco> tacoById(@PathVariable("id") Long id) {
         Optional<Taco> optionalTaco = tacoRepository.findById(id);
         return optionalTaco.map(taco -> new ResponseEntity<>(taco, HttpStatus.OK)).orElseGet(() -> new ResponseEntity<>(null, HttpStatus.NOT_FOUND));
+    }
+
+    @PostMapping(consumes = "application/json")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Taco postTaco(@RequestBody Taco taco) {
+        return tacoRepository.save(taco);
+    }
+
+    @PatchMapping(path = "/{orderId}", consumes = "application/json")
+    public TacoOrder patchOrder(@PathVariable("orderId") Long orderId, @RequestBody TacoOrder patch) {
+
+        TacoOrder tacoOrder = orderRepository.findById(orderId).get();
+
+        if (patch.getDeliveryName() != null) {
+            tacoOrder.setDeliveryName(patch.getDeliveryName());
+        }
+        if (patch.getDeliveryStreet() != null) {
+            tacoOrder.setDeliveryStreet(patch.getDeliveryStreet());
+        }
+        if (patch.getDeliveryCity() != null) {
+            tacoOrder.setDeliveryCity(patch.getDeliveryCity());
+        }
+        if (patch.getDeliveryState() != null) {
+            tacoOrder.setDeliveryState(patch.getDeliveryState());
+        }
+        if (patch.getDeliveryZip() != null) {
+            tacoOrder.setDeliveryZip(patch.getDeliveryZip());
+        }
+        if (patch.getCcNumber() != null) {
+            tacoOrder.setCcNumber(patch.getCcNumber());
+        }
+        if (patch.getCcExpiration() != null) {
+            tacoOrder.setCcExpiration(patch.getCcExpiration());
+        }
+        if (patch.getCcCVV() != null) {
+            tacoOrder.setCcCVV(patch.getCcCVV());
+        }
+        return orderRepository.save(tacoOrder);
+    }
+
+    @DeleteMapping("/{orderId}")
+    public void deleteOrder(@PathVariable("orderId") Long orderId) {
+        try {
+            tacoRepository.deleteById(orderId);
+        } catch (EmptyResultDataAccessException e) {}
     }
 }
